95e2f7ee226fcc4d615c1145e267b00a6e4e2846
nguyen hoang <overlord.x.king@gmail.com>
2023-05-27 07:15:35 +0700

    fix cros

--- src/main/java/com/example/userserver/config/SecurityConfig.java
+++ src/main/java/com/example/userserver/config/SecurityConfig.java
@@ -18,11 +18,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -111,15 +106,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://itemserver-production.up.railway.app/"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
 }
